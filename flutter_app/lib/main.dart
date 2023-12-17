22e84a70ef4ab72603c6c7623d9340af2ab5a39a
khanalmanoj <48397534+khanalmanoj@users.noreply.github.com>
2023-12-17 13:20:37 +0545

    3rd_commit

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -14,49 +15,87 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('JSON Retrieval Example'),
         ),
-        body: FutureBuilder(
-          future: fetchData(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {
-              List<dynamic> dataList = snapshot.data as List<dynamic>;
-
-              return ListView.builder(
-                itemCount: dataList.length,
-                itemBuilder: (context, index) {
-                  Map<String, dynamic> jsonData = dataList[index] as Map<String, dynamic>;
-
-                  return ListTile(
-                    title: Text(jsonData['food_name']),
-                    subtitle: Text(jsonData['desc']),
-                    trailing: Text(jsonData['time']),
-                    leading: Image.network(jsonData['image']),
-                    onTap: () {
-                      // Add any action you want when the ListTile is tapped
-                    },
-                  );
-                },
-              );
-            }
-          },
-        ),
+        body: MyHomePage(),
       ),
     );
   }
 }
 
-Future<List<dynamic>> fetchData() async {
-  final response = await http.get(
-    Uri.parse("http://127.0.0.1:8000/api/food/"),
-    // Replace "your-api-endpoint" with the actual endpoint for retrieving JSON data
-  );
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  if (response.statusCode == 200) {
-    return json.decode(response.body);
-  } else {
-    throw Exception('Failed to load data');
+class _MyHomePageState extends State<MyHomePage> {
+  List<dynamic> dataList = [];
+  String jsonDataString = "";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      final response = await http.get(
+        Uri.parse("http://127.0.0.1:8000/api/food/"),
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          dataList = json.decode(response.body);
+        });
+
+        // Now that data is available, generate QR code
+        generateQRCode();
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      print("Exception: $e");
+    }
+  }
+
+  void generateQRCode() {
+    // Assuming your JSON data structure is different, modify accordingly
+    if (dataList.isNotEmpty) {
+      Map<String, dynamic> jsonData = dataList.first as Map<String, dynamic>;
+      jsonDataString = jsonEncode(jsonData);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        if (dataList.isEmpty)
+          CircularProgressIndicator()
+        else
+          Expanded(
+            child: ListView.builder(
+              itemCount: dataList.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> jsonData = dataList[index] as Map<String, dynamic>;
+
+                return ListTile(
+                  title: Text(jsonData['food_name']),
+                  subtitle: Text(jsonData['desc']),
+                  trailing: Text(jsonData['time']),
+                  leading: Image.network(jsonData['image']),
+                  onTap: () {
+                    // Add any action you want when the ListTile is tapped
+                  },
+                );
+              },
+            ),
+          ),
+        QrImageView(
+          data: jsonDataString,
+          version: QrVersions.auto,
+          size: 200.0,
+        ),
+      ],
+    );
   }
 }
