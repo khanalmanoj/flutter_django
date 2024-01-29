c02dea5ebc7e6a646d659b59bd8da8f2a3f905b9
khanalmanoj <48397534+khanalmanoj@users.noreply.github.com>
2024-01-29 20:20:17 +0545

    Folder_structure_changed

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/new/CartPage.dart';
 import 'package:flutter_app/new/FoodViewModel.dart';
 import 'package:flutter_app/new/MyHomePage.dart';
-
 import 'package:provider/provider.dart';
 
 void main() {
